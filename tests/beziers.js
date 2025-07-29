72475ee69592658f907f4425a615bb8f53ed4593
Christian Swinehart <drafting@samizdat.co>
2025-07-29 13:16:02 -0400

    tweak palette

--- tests/beziers.js
+++ tests/beziers.js
@@ -7,7 +7,8 @@ export default function drawSpaghetti({createCanvas, getBitmap}){
         count = 150,
         points = 10,
         rng = seed(process.env.SEED || 123),
-        coord = () => rng.intBetween(0, size)
+        coord = () => rng.intBetween(0, size),
+        colors = ["#59a14f", "#e15759", "#edc949", "#4e79a7", "#76b7b2", "black"]
 
     for (let ln=0; ln<count; ln++){
         ctx.beginPath()
@@ -21,7 +22,7 @@ export default function drawSpaghetti({createCanvas, getBitmap}){
         }
 
         ctx.lineWidth = [1, 3, 5, 10, 20, 40][rng.intBetween(0,5)]
-        ctx.strokeStyle = ['white', 'black', 'skyblue'][rng.intBetween(0,2)]
+        ctx.strokeStyle = colors[rng.intBetween(0, colors.length-1)]
         ctx.globalAlpha = rng.floatBetween(0.1, 0.5)
         ctx.stroke()
     }
